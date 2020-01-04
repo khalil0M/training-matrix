f5e8d90cd771cdbcf33500c3d9286770cc3e2496
bellouki <outhmane.bellouki@gmail.com>
2020-01-04 20:41:56 +0100

    first commit

---
+++ src/main/java/com/humanup/matrix/training/trainingmatrix/TrainingMatrixApplication.java
@@ -0,0 +1,13 @@
+package com.humanup.matrix.training.trainingmatrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingMatrixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingMatrixApplication.class, args);
+	}
+
+}
